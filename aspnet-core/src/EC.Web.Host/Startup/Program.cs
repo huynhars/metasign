7938a065a63c70923c7f73bbd2d8b903fc0ed234
huynhars <badhuynh4@gmail.com>
2026-09-17 14:40:08 +0700

    Fix Dockerfile to use .NET 6 SDK

--- aspnet-core/src/EC.Web.Host/Startup/Program.cs
+++ aspnet-core/src/EC.Web.Host/Startup/Program.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using Abp.AspNetCore.Dependency;
+using Abp.Dependency;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using System;
 
 namespace EC.Web.Host.Startup
 {
@@ -6,11 +10,20 @@ namespace EC.Web.Host.Startup
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            CreateHostBuilder(args).Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+        internal static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            AppContext.SetSwitch("Npgsql.DisableDateTimeInfinityConversions", true);
+            AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+
+            return Microsoft.Extensions.Hosting.Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                })
+                .UseCastleWindsor(IocManager.Instance.IocContainer);
+        }
     }
 }
