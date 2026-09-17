2471ee499a5e718dc48ca148a2106d607493ff7a
huynhars <badhuynh4@gmail.com>
2026-09-17 14:24:09 +0700

    Fix Dockerfile to use .NET 6 SDK

--- aspnet-core/src/EC.Web.Host/Startup/Program.cs
+++ aspnet-core/src/EC.Web.Host/Startup/Program.cs
@@ -1,10 +1,5 @@
 ﻿using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using EC.EntityFrameworkCore;
-using System;
 using MSHost = Microsoft.Extensions.Hosting.Host;
 
 namespace EC.Web.Host.Startup
@@ -13,26 +8,7 @@ namespace EC.Web.Host.Startup
     {
         public static void Main(string[] args)
         {
-            var host = CreateHostBuilder(args).Build();
-
-            using (var scope = host.Services.CreateScope())
-            {
-                var services = scope.ServiceProvider;
-                var logger = services.GetRequiredService<ILogger<Program>>();
-                try
-                {
-                    var dbContext = services.GetRequiredService<ECDbContext>();
-                    dbContext.Database.Migrate();
-                    logger.LogInformation("Database migration completed successfully.");
-                }
-                catch (Exception ex)
-                {
-                    logger.LogError(ex, "An error occurred while migrating the database.");
-                    throw;
-                }
-            }
-
-            host.Run();
+            CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
